c034f8e3ae99ff712537a07c699072ab9148aa20
Melissa Saldana <melissasaldana@Melissas-MacBook-Air.local>
2018-03-22 22:55:51 -0400

    first part

---
+++ liri.js
@@ -0,0 +1,36 @@
+//junk I need to start
+require("dotenv").config();
+var Twitter = require('twitter');
+var spotify = require('spotify');
+var request = require('request');
+
+
+// import keys
+var fs = require('fs');
+var keys = require('./keys.js');
+
+//first thing is twiter 
+function Tweets(){
+	console.log("your tweets");
+	var client = new twitter({
+		consumer_key: keys.twitterKeys.consumer_key,
+		consumer_secret: keys.twitterKeys.consumer_secret,
+		access_token_key: keys.twitterKeys.access_token_key,
+		access_token_secret: keys.twitterKeys.access_token_secret
+	});
+	var parameters = {
+		screen_name: 'meliscode',
+		count: 20
+	};
+
+	//call the get method on our client variable twitter instance
+	client.get('statuses/user_timeline', parameters, function(error, tweets, response){
+		if (!error) {
+	        for (i=0; i<tweets.length; i++) {
+	            var returnedData = ('Number: ' + (i+1) + '\n' + tweets[i].created_at + '\n' + tweets[i].text + '\n');
+	            console.log(returnedData);
+	            console.log("-------------------------");
+	        }
+	    };
+	});
+};//end fetchTweets;
